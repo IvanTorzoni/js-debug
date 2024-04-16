978dec26bdd89d1b387c89c333039a2c2159f634
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-16 15:17:41 +0200

    feat: debug of exercise in object folder

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -9,8 +9,86 @@
 ​
 *******************************************************************************/
 
+// Spiegazione.
+// Sono presenti errori sintattici e logici
+// Il risultato finale deve smistare le macchine in tre array, uno per le macchine diesel, uno a benzina e uno per tutte le restanti
 
 // ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+// const cars = [
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Fiesta',
+//         type: 'diesel'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'A1',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Golf',
+//         type: 'Benzina'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Panda',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Multipla',
+//         type: 'GPL'
+//     },
+//     {
+//         manufacturer: 'Tesla',
+//         model: 'Model 3',
+//         type: 'elettrico'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Polo',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Kuga',
+//         type: 'Diesel'
+//     },
+//     {
+//         manufacturer: 'Seat',
+//         model: 'Ibiza',
+//         type: 'metano'
+//     }
+//     {
+//         manufacturer: 'Audi',
+//         model: 'R8',
+//         type: 'Benzina'
+//     },
+// ];
+
+// const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+
+// const dieselCars = cars.filter( (auto) => {
+//     auto.type === 'diesel';
+// });
+
+// const otherCars = cars.filter( (auto) => {
+//     return auto.type !== 'benzina' || auto.type !== 'diesel';
+// });
+
+// console.log('Auto a benzina');
+// console.log('*******************************');
+// console.log(gasolineCars);
+
+// console.log('Auto a diesel');
+// console.log('*******************************');
+// console.log(dieselCars);
+
+// console.log('Tutte le altre auto');
+// console.log(otherCars);
+
+//Soluzione (commenti messi a fianco delle modifiche)
 const cars = [
     {
         manufacturer: 'Ford',
@@ -50,13 +128,13 @@ const cars = [
     {
         manufacturer: 'Ford',
         model: 'Kuga',
-        type: 'Diesel'
+        type: 'diesel'              //Sistemata la stringa inserendo la D in minuscolo
     },
     {
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },                              //Inserita la vigola
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,14 +142,12 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+let gasolineCars = cars.filter( (auto) => auto.type === 'benzina');  //sugar syntax errata
 
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
-});
+let dieselCars = cars.filter( (auto) => auto.type === 'diesel');    //errore di parentesi, errore di sintassi (; in più)
 
-const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+let otherCars = cars.filter( (auto) => {
+    return auto.type !== 'benzina' && auto.type !== 'diesel';       //mettere and al posto di or
 });
 
 console.log('Auto a benzina');
